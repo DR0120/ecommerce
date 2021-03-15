8518e3c6af24bf6e949bcb368982ac247b57210d
DR0120 <67388865+DR0120@users.noreply.github.com>
2021-03-15 15:50:41 -0400

    Update layout2.blade.php

--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -45,7 +45,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Default to the left -->
     <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
   </footer> --}}
-  @include('layouts.footer')
+ 
 </div>
 <!-- ./wrapper -->
 
